fd1e443409c428e712c7d6d5fe891aa183abc3ac
Craig Dempsey <craigdempsey@gmail.com>
2017-08-19 17:55:42 -0400

    db stuff

--- app.js
+++ app.js
@@ -25,9 +25,21 @@ app.use(session({
 
 var db = pgp('postgres://macbook@localhost:5432/moongarden');
 
-let port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
+var pg = require('pg');
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 //===========================================================
 
 app.get('/', function(req, res){
