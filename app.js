688b61ea1e78390c471d737efa4084bdb6f59618
Craig Dempsey <craigdempsey@gmail.com>
2017-08-21 12:06:38 -0400

    will it db?

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.use(session({
 }))
 
 var port = process.env.PORT || 8080;
-var db = pgp('postgres://gjtqmpggmgeuzp:a223b0a98427908099bdde72706ea3af2929bd0f3dde6a1282696683a4b47e24@ec2-23-23-227-188.compute-1.amazonaws.com:5432/d1ev8ka848bkce') || pgp('postgres://macbook@localhost:5432/moongarden');
+
+// EITHER ONE?
+// var db = pgp('postgres://gjtqmpggmgeuzp:a223b0a98427908099bdde72706ea3af2929bd0f3dde6a1282696683a4b47e24@ec2-23-23-227-188.compute-1.amazonaws.com:5432/d1ev8ka848bkce') || pgp('postgres://macbook@localhost:5432/moongarden');
 
 //===========================================================
 //===========================================================
@@ -58,17 +60,17 @@ var db = pgp('postgres://gjtqmpggmgeuzp:a223b0a98427908099bdde72706ea3af2929bd0f
 // pool.end();
 //===========================================================
 
-pg.defaults.ssl = true;
+// pg.defaults.ssl = true;
 
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+  console.log('Connected through heroku config db url');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
+  // client
+  //   .query('SELECT table_schema,table_name FROM information_schema.tables;')
+  //   .on('row', function(row) {
+  //     console.log(JSON.stringify(row));
+  //   });
 });
 
 //===========================================================
