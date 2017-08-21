5aa15f0cfa9e5fb9238a7ad79a1d3500706f4cbf
Craig Dempsey <craigdempsey@gmail.com>
2017-08-21 14:30:40 -0400

    whim and a wish

--- app.js
+++ app.js
@@ -25,10 +25,7 @@
   }))
 
   var port = process.env.PORT || 8080;
-  // var port = 3000;
-
-  // EITHER ONE?
-  // var db = pgp('postgres://gjtqmpggmgeuzp:a223b0a98427908099bdde72706ea3af2929bd0f3dde6a1282696683a4b47e24@ec2-23-23-227-188.compute-1.amazonaws.com:5432/d1ev8ka848bkce') || pgp('postgres://macbook@localhost:5432/moongarden');
+  // var port = 8080;
 
   //===========================================================
   //===========================================================
@@ -49,13 +46,12 @@
   //===========================================================
   // CONNECTION
 
-
   pg.defaults.ssl = true;
 
   var pool = new pg.Pool()
 
   // connection using created pool
-  pool.connect(function(err, client, done) {
+  pool.connect(process.env.DATABASE_URL,function(err, client, done) {
     if (err) {
       console.log('not connecting');
     }
